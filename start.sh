180e1e3b34b8062e81082cb72dd0bec65f29df7a
Harshal Purohit <82707364+HarshalPurohitEdits@users.noreply.github.com>
2024-02-01 21:22:00 +0530

    TheMovieProviderBot

---
+++ start.sh
@@ -0,0 +1,12 @@
+if [ -z $UPSTREAM_REPO ]
+then
+  echo "Cloning main Repository"
+  git clone https://github.com/HarshalPurohitEdits/TheMovieProviderBot.git /TheMovieProviderBot 
+else
+  echo "Cloning Custom Repo from $UPSTREAM_REPO "
+  git clone $UPSTREAM_REPO /TheMoviesProviderBot
+fi
+cd /THALAPATHY-FILTER-BOT 
+pip3 install -U -r requirements.txt
+echo "Starting TheMovieProviderBot...."
+python3 bot.py
