ed48ebddbfa5cde9aaaa20917e247f6d83e46add
Harshal Purohit <82707364+HarshalPurohitEdits@users.noreply.github.com>
2024-02-08 20:09:05 +0530

    TheMovieProviderBot

--- database/users_chats_db.py
+++ database/users_chats_db.py
@@ -204,9 +204,15 @@ class Database:
             {"id": user_id}, {"$set": {"expiry_time": None}}
         )
 
-    async def give_free_trial(self, userid):
+    async def check_trial_status(self, user_id):
+        user_data = await self.get_user(user_id)
+        if user_data:
+            return user_data.get("has_free_trial", False)
+        return False
+
+    async def give_free_trial(self, user_id):
         #await set_free_trial_status(user_id)
-        user_id = userid
+        user_id = user_id
         seconds = 5*60         
         expiry_time = datetime.datetime.now() + datetime.timedelta(seconds=seconds)
         user_data = {"id": user_id, "expiry_time": expiry_time, "has_free_trial": True}
